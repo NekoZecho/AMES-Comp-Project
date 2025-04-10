8a1d6abce7787750b849e3fff3e40d0391d8a172
NekoZechon <83736697+NekoZecho@users.noreply.github.com>
2025-04-10 10:25:23 -0700

    Player Attack System 0.1

--- Assets/Zechon's Assets/Scripts/Player/Attacks/Attack_System.cs
+++ Assets/Zechon's Assets/Scripts/Player/Attacks/Attack_System.cs
@@ -12,15 +12,21 @@ public class Attack_System : MonoBehaviour
 
     [Header("Cooldowns / Debounces")]
     [SerializeField]
-    float LAttackDuration;
+    float lAttackDuration;
+    float lAtkD;
     [SerializeField]
-    float HAttackDuration;
+    float hAttackDuration;
+    float hAtkD;
     [SerializeField]
     float blockRecup;
+    float nyaBR;
     [SerializeField]
     float decisionDuration;
+    [SerializeField]
+    float comboResiliance;
 
     [Header("Hits Info")]
+    public float timeSinceLastHit;
     public int consecutiveHits;
     public int lightHits;
     public int heavyHits;
@@ -32,18 +38,174 @@ public class Attack_System : MonoBehaviour
     Collider LeftHand;
 
     [Header("Bools")]
-    public bool attacking;
-    public bool blocking;
-    public bool staggered;
-    public bool dodging;
+    private bool attacking;
+    private bool blocking;
+    private bool dodging;
+    private bool stunned;
+
+    [Header("Player Attack")]
+    public PlayerAttackState pAState;
+    public enum PlayerAttackState
+    {
+        passive,
+        attacking,
+        blocking,
+        dodging,
+        stunned
+    }
+    public bool rightHandActive;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+
+        pAState = PlayerAttackState.passive;
+
+        rightHandActive = true;
+
+        lAtkD = lAttackDuration;
+        hAtkD = hAttackDuration;
+        nyaBR = blockRecup;
+
+        timeSinceLastHit = 0;
     }
 
     void Update()
     {
-        
+        Timers();
+        Attack();
+        StateHandler();
+    }
+
+    private void StateHandler()
+    {
+        //For Enemy Attack / Detection Scripts
+        if (attacking)
+        {
+            pAState = PlayerAttackState.attacking;
+        }
+        else if (blocking)
+        {
+            pAState = PlayerAttackState.blocking;
+        }
+        else if (dodging)
+        {
+            pAState = PlayerAttackState.dodging;
+        }
+        else if (stunned)
+        {
+            pAState= PlayerAttackState.stunned;
+        }
+        else
+        {
+            pAState = PlayerAttackState.passive;
+        }
+    }
+
+    private void Attack()
+    {
+        //light attack, duh
+        if (Input.GetKeyDown(lightAttack))
+        {
+            if (!stunned && !blocking && !attacking && lAttackDuration == 0 && hAttackDuration == 0)
+            {
+                attacking = true;
+                lAttackDuration = lAtkD;
+                timeSinceLastHit = 0;
+                consecutiveHits++;
+                lightHits++;
+                Debug.Log("Light Attack!");
+            }
+        }
+        //heavy attack, (heavy tf2 reference!!!)
+        if (Input.GetKeyDown(heavyAttack))
+        {
+            if (!stunned && !blocking && !attacking && lAttackDuration == 0 && hAttackDuration == 0)
+            {
+                attacking = true;
+                hAttackDuration = hAtkD;
+                timeSinceLastHit = 0;
+                consecutiveHits++;
+                heavyHits++;
+                Debug.Log("Heavy Attack!");
+            }
+        }
+        //blocking
+        if (Input.GetKeyDown(block))
+        {
+            if (!stunned && !attacking && blockRecup == 0)
+            {
+                blocking = true;
+            }
+        }
+
+    }
+
+    private void Timers()
+    {
+        //Cooldown Timers and stuff
+        if (lAttackDuration != 0)
+        {
+            lAttackDuration -= Time.deltaTime;
+            if (lAttackDuration < 0)
+            {
+                lAttackDuration = 0;
+            }
+        }
+        if (hAttackDuration != 0)
+        {
+            hAttackDuration -= Time.deltaTime;
+            if (hAttackDuration < 0)
+            {
+                hAttackDuration = 0;
+            }
+        }
+        if (blockRecup != 0)
+        {
+            blockRecup -= Time.deltaTime;
+            if (blockRecup < 0)
+            {
+                blockRecup = 0;
+            }
+        }
+        if (lAttackDuration == 0 && hAttackDuration == 0)
+        {
+            attacking = false;
+        }
+
+        //Combo tracking timers
+        timeSinceLastHit += Time.deltaTime;
+        if (timeSinceLastHit > comboResiliance)
+        {
+            consecutiveHits = 0;
+        }
+    }
+
+    private void unAttack()
+    {
+        attacking = false;
+        lAttackDuration = lAtkD;
+    }
+
+    private void lHit()
+    {
+        lAttackDuration = lAtkD;
+        timeSinceLastHit = 0;
+    }
+    private void hHit()
+    {
+        hAttackDuration = hAtkD;
+        timeSinceLastHit = 0;
+    }
+
+    private void unBlock()
+    {
+        blocking = false;
+    }
+
+    private void defaultStance()
+    {
+        unAttack();
+        unBlock();
     }
 }
