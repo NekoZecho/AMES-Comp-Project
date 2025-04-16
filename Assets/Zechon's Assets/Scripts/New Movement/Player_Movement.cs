cfeb78f9a8ef4d33b05341f4f0f7f157686cfc93
NekoZechon <83736697+NekoZecho@users.noreply.github.com>
2025-04-16 10:29:12 -0700

    Third Person System

--- Assets/Zechon's Assets/Scripts/New Movement/Player_Movement.cs
+++ Assets/Zechon's Assets/Scripts/New Movement/Player_Movement.cs
@@ -6,15 +6,42 @@ public class Player_Movement : MonoBehaviour
 {
     [Header("References")]
     public Transform orientation;
+    [SerializeField]
+    Animator anim;
+    [SerializeField]
+    CapsuleCollider clldr;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
 
     [Header("Movement")]
-    public float moveSpeed;
+    private float moveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
     public float groundDrag;
+
+    [Header("Jumping")]
+    public float jumpForce;
+    public float jumpCD;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
 
     [Header("Ground Check")]
     public float plyrHeight;
     public LayerMask thisIsGround;
     bool grounded;
+
+    [Header("Slope Handling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    private bool exitingSlope;
 
     float horizInput;
     float vertInput;
@@ -22,11 +49,24 @@ public class Player_Movement : MonoBehaviour
     Vector3 moveDirection;
 
     Rigidbody rb;
+    
+    public MovementState state;
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        crouching,
+        air
+    }
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+
+        readyToJump = true;
+
+        startYScale = clldr.height;
     }
 
     void Update()
@@ -35,6 +75,8 @@ public class Player_Movement : MonoBehaviour
         grounded = Physics.Raycast(transform.position, Vector3.down, plyrHeight * 0.5f + 0.2f, thisIsGround);
 
         InputTimeWahoo();
+        DontSpeed();
+        StateHandler();
 
         //this ground be havin friction (drag)
         if (grounded)
@@ -57,6 +99,26 @@ public class Player_Movement : MonoBehaviour
     {
         horizInput = Input.GetAxisRaw("Horizontal");
         vertInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+
+            Invoke(nameof(JumpReset), jumpCD);
+        }
+
+        if (Input.GetKeyDown(crouchKey))
+        {
+            anim.SetBool("Crouching", true);
+            clldr.height = startYScale * crouchYScale;
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyUp(crouchKey))
+        {
+            clldr.height = startYScale;
+        }
     }
 
     void PlayerMove()
@@ -64,17 +126,109 @@ public class Player_Movement : MonoBehaviour
         //move calc dir
         moveDirection = orientation.forward * vertInput + orientation.right * horizInput;
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        if (OnSlope() && !exitingSlope)
+        {
+            rb.AddForce(GetSlopeMDirection() * moveSpeed * 20f, ForceMode.Force);
+
+            if (rb.velocity.y > 0)
+            {
+                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+            }
+        }
+
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+
+        else if(!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+
+        rb.useGravity = !OnSlope();
+    }
+
+    void StateHandler()
+    {
+        if (Input.GetKeyDown(crouchKey))
+        {
+            state = MovementState.crouching;
+            moveSpeed = crouchSpeed;
+        }
+        if(grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            moveSpeed = sprintSpeed;
+            anim.SetBool("Walking", false);
+        }
+        else if(grounded)
+        {
+            state = MovementState.walking;
+            moveSpeed = walkSpeed;
+            anim.SetBool("Walking", true);
+        }
+        else
+        {
+            state = MovementState.air;
+        }
     }
 
     void DontSpeed()
     {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        if(flatVel.magnitude > moveSpeed)
+        //speeding on slopes aint allowed, so we doing this bit too
+        if (OnSlope() && !exitingSlope)
         {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            if (rb.velocity.magnitude > moveSpeed)
+            {
+                rb.velocity = rb.velocity.normalized * moveSpeed;
+            }
         }
+
+        else
+        {
+            Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+            //if speeding (very scary) do this stuff
+            if(flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+            }
+        }
+
+    }
+
+    void Jump()
+    {
+        exitingSlope = true;
+
+        //reset yo verticality speed
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void JumpReset()
+    {
+        readyToJump = true;
+
+        exitingSlope = false;
+    }
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, plyrHeight * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+
+        return false;
+    }
+
+    private Vector3 GetSlopeMDirection()
+    {
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
     }
 }
--- Assets/Zechon's Assets/Scripts/New Movement/ThirdPersonCam.cs
+++ Assets/Zechon's Assets/Scripts/New Movement/ThirdPersonCam.cs
@@ -9,9 +9,20 @@ public class ThirdPersonCam : MonoBehaviour
     public Transform player;
     public Transform plyrObj;
     public Rigidbody rb;
+    public Transform combatLookAt;
+    public GameObject thirdPersonCam;
+    public GameObject combatCam;
 
     [Header("Floats")]
     public float rotateSpeed;
+
+
+    public CameraStyle currentStyle;
+    public enum CameraStyle
+    {
+        Basic,
+        Combat
+    }
 
     private void Start()
     {
@@ -21,17 +32,54 @@ public class ThirdPersonCam : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            SwitchCameraStyle(CameraStyle.Basic);
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            SwitchCameraStyle(CameraStyle.Combat);
+        }
+
         Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
-        //You spin me right round, baby right round
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-        if (inputDir != Vector3.zero)
+        if (currentStyle == CameraStyle.Basic)
         {
-            plyrObj.forward = Vector3.Slerp(plyrObj.forward, inputDir.normalized, Time.deltaTime * rotateSpeed);
+            //You spin me right round, baby right round
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+            if (inputDir != Vector3.zero)
+            {
+                plyrObj.forward = Vector3.Slerp(plyrObj.forward, inputDir.normalized, Time.deltaTime * rotateSpeed);
+            }
         }
+
+        else if (currentStyle == CameraStyle.Combat)
+        {
+            Vector3 dirToCombatLookAt = combatLookAt.position - new Vector3(transform.position.x, combatLookAt.position.y, transform.position.z);
+            orientation.forward = dirToCombatLookAt.normalized;
+
+           plyrObj.forward = dirToCombatLookAt.normalized;
+        }
+    }
+
+    private void SwitchCameraStyle(CameraStyle newStyle)
+    {
+        combatCam.SetActive(false);
+        thirdPersonCam.SetActive(false);
+
+        if (newStyle == CameraStyle.Basic)
+        {
+            thirdPersonCam.SetActive(true);
+        }
+        if (newStyle == CameraStyle.Combat)
+        {
+            combatCam.SetActive(true);
+        }
+
+        currentStyle = newStyle;
     }
 }
